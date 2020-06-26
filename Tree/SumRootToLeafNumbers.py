b4e36138acff999b140fc9638260e48ea082c839
RobinKongNingLo <46904895+RobinKongNingLo@users.noreply.github.com>
2020-06-26 17:34:11 -0400

    Update SumRootToLeafNumbers.py

--- Tree/SumRootToLeafNumbers.py
+++ Tree/SumRootToLeafNumbers.py
@@ -3,15 +3,15 @@ class Solution:
         if not root:
             return 0
         self.res = 0
-        self.sumPath(root, '')
+        self.sumPath(root, 0)
         return self.res
         
     def sumPath(self, root, path):
-        path += str(root.val)
+        path = path * 10 + root.val
         if root.left:
             self.sumPath(root.left, path)
         if root.right:
             self.sumPath(root.right, path)
         if not root.left and not root.right:
-            self.res += int(path)
+            self.res += path
             return
